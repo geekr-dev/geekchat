bb44a9aebba76c9673d0ca1b8e9b6b3c620216a9
geekr <geekr@hey.com>
2023-03-09 23:13:08 +0800

    代码修复

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,6 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-
+            ->header('Access-Control-Allow-Origin', '*');
     }
 }
