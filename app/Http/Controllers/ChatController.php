fd5ff6eb60844e034eaf04f1b2f3a59dd77ace5f
geekr <geekr@hey.com>
2023-03-09 14:26:51 +0800

    基于Inertia+Vue3重构GeekChat功能

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -5,23 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use GeekrOpenAI\Laravel\Facades\OpenAI;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
 
 class ChatController extends Controller
 {
+    public function index()
+    {
+        $messages = collect(session('messages', []))->reject(fn ($message) => $message['role'] === 'system');
+        return Inertia::render('Welcome', [
+            'messages' => $messages->toArray(),
+        ]);
+    }
+
     /**
      * Handle the incoming prompt.
      */
     public function chat(Request $request): RedirectResponse
     {
         $request->validate([
-            'message' => 'required|string'
+            'prompt' => 'required|string'
         ]);
 
         $messages = $request->session()->get('messages', [
             ['role' => 'system', 'content' => 'You are GeekChat - A ChatGPT clone. Answer as concisely as possible.']
         ]);
 
-        $messages[] = ['role' => 'user', 'content' => $request->input('message')];
+        $messages[] = ['role' => 'user', 'content' => $request->input('prompt')];
 
         $response = OpenAI::chat()->create([
             'model' => 'gpt-3.5-turbo',
@@ -32,7 +42,7 @@ class ChatController extends Controller
 
         $request->session()->put('messages', $messages);
 
-        return redirect('/');
+        return Redirect::to('/');
     }
 
     /**
@@ -42,6 +52,6 @@ class ChatController extends Controller
     {
         $request->session()->forget('messages');
 
-        return redirect('/');
+        return Redirect::to('/');
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import Markdown from 'vue3-markdown-it';
+
+// 代码高亮
+// import 'highlight.js/styles/github.css';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .component('markdown', Markdown)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
